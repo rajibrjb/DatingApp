ee9ae8488007df4f12ccf4ca3e4a9d0192b94636
rajibrjb <rajib.mrn@gmail.com>
2018-12-02 10:49:38 +1100

    Client side login and register

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -6,20 +6,26 @@ import { ValueComponent } from './value/value.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import {FormsModule} from '@angular/forms';
-import { AuthService } from './services/auth.service';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule
    ],
-   providers: [AuthService],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -14,11 +14,22 @@ export class NavComponent implements OnInit {
   }
 
   login() {
+    console.log(this.model);
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in successfully');
     }, error => {
       console.log('Faild to login');
     } );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
   }
 
 }
